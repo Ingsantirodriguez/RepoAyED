316f3ad8de86c969ef256e62d60d4bd521bcd726
Santiago Rodriguez <santirodriguez15598@gmail.com>
2021-03-29 12:12:50 -0300

    Hanoi

--- ClaseP3/Hanoi.cpp
+++ ClaseP3/Hanoi.cpp
@@ -13,11 +13,11 @@ int main(int argc, char *argv[])
     return EXIT_SUCCESS;
 }
 
-void hanoi(char a, char c, char b, int n)
+void hanoi(char a, char c, char b, int n)	// La funcion mueve el disco actual de la posicion en el primer parametro a la del segundo parametro
 {
     if(n>0){
-        hanoi(a,b,c,n-1);   //mover los n-1 primeros discos de a >> b
+        hanoi(a,b,c,n-1);					//mover los n-1 primeros discos de a >> b
         cout<<"Mover "<<n<<" desde "<<a<<" hasta "<<c<<endl;    //el ultimo disco lo muevo de a >> c
-        hanoi(b,c,a,n-1);   // mover los n-1 primeros discos de b >> c
+        hanoi(b,c,a,n-1);					// mover los n-1 primeros discos de b >> c
      }
 }
