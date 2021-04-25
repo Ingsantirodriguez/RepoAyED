a74f1163a86719c62f574946f3ac1342a1b89092
Ingsantirodriguez <76255289+Ingsantirodriguez@users.noreply.github.com>
2021-04-25 17:35:38 -0300

    Inicio de TP1, pasar al otro repositorio

---
+++ Trabajo1/BNaval.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+struct coord            // Tipo de dato coordenada
+{
+    int x;
+    int y;
+};
+
+class Barco
+{
+private:
+    coord pos[4];       // Posiciones de cada casillero que ocupa el barco.
+                        // Si ocupa menos de 4 casilleros, los vacios se llenan con null.
+    int tipo;           // El tipo de barco se distingue por la cantidad de casilleros que ocupa.
+};
+
+class Pila
+{
+private:
+    coord
+}
+
+void main ()
+{
+    bool mapa[10][10];  // El arreglo es un mapa bidimensional de 10x10 de tipo booleano.
+}
+
+
